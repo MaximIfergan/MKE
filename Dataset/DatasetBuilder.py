ccfd1dcdfd4ecf5ab91f9ecb96b7e50676d28664
maxim <maxim758@gmail.com>
2024-01-10 13:17:33 +0200

    Advance in dataset construction

--- Dataset/DatasetBuilder.py
+++ Dataset/DatasetBuilder.py
@@ -45,21 +45,6 @@ PROMPT_TEMPLATES = {"birth_year":
                          "ar": {"M": ["ولد {} في مدينة", "مدينة ميلاد {} هي", "تمت ولادة باخ في مدينة"],
                                 "F": ["ولدت {} في مدينة", "مدينة ميلاد {} هي", "تمت ولادة باخ في مدينة"]}
                          },
-                    "occupation": {"en": {"M": ["{} worked as a ", "The profession of {} was ",
-                                                "{}'s main occupation was as a "],
-                                          "F": ["{} worked as a ", "The profession of {} was ",
-                                                "{}'s main occupation was as a "]},
-                                   "fr": {"M": ["{} a travaillé comme ", "Le métier de {} était ",
-                                                "La principale occupation de {} était "],
-                                          "F": ["{} a travaillé comme ", "Le métier de {} était ",
-                                                "La principale occupation de {} était "]},
-                                   "ru": {"M": ["", "", ""],
-                                          "F": ["", "", ""]},  # TODO complete!
-                                   "he": {"M": ["{} עבד כ", "המקצוע של {} היה ", "העיסוק המרכזי של {} היה "],
-                                          "F": ["{} עבדה כ", "המקצוע של {} היה ", "העיסוק המרכזי של {} היה "]},
-                                   "ar": {"M": ["", "", ""],
-                                          "F": ["", "", ""]}  # TODO complete!
-                                   },
                     "death_year": {"en": {"M": ["{} died in the year ", "The death year of {} is ",
                                                 "{}'s death took place in the year "],
                                           "F": ["{} died in the year ", "The death year of {} is ",
@@ -69,12 +54,30 @@ PROMPT_TEMPLATES = {"birth_year":
                                           "F": ["{} est née en l'an ", "L'année de la mort de {} est ",
                                                 "La mort de {} a eu lieu en l'an "]},
                                    "ru": {"M": ["", "", ""],
-                                          "F": ["", "", ""]},  # TODO complete!
-                                   "he": {"M": ["{} מת בשנת ", "שנת מותו של {} היא ", "מותו של {} התרחש בשנת "],
-                                          "F": ["{} מתה בשנת ", "שנת מותה של {} היא ", "מותה של {} התרחש בשנת "]},
+                                          "F": ["", "", ""]},
+                                   "he": {"M": ["מת בשנת {}", "שנת מותו של {} היא ", "מותו של {} התרחש בשנת "],
+                                          "F": ["מתה בשנת {}", "שנת מותה של {} היא ", "מותה של {} התרחש בשנת "]},
                                    "ar": {"M": ["توفي {} عام", "سنة وفاة {} هي", "حدثت وفاة {} في عام"],
                                           "F": ["توفيت {} عام", "سنة وفاة {} هي", "حدثت وفاة {} في عام"]}
-                                   }
+                                   },
+                    "sport": {"en": {"M": ["{} professionally plays the sport of",
+                                           "The sport that {} is associated with is",
+                                           "{} participates in the sport of"],
+                                     "F": ["{} professionally plays the sport of",
+                                           "The sport that {} is associated with is",
+                                           "{} participates in the sport of"]},
+                              "fr": {"M": ["{} joue professionnellement au sport du",
+                                           "Le sport avec lequel {} est associé est le", "{} participe au sport du"],
+                                     "F": ["{} joue professionnellement au sport du",
+                                           "Le sport auquel {} est associée est le", "{} participe au sport du"]},
+                              "ru": {"M": ["", "", ""],
+                                     "F": ["", "", ""]},
+                              "he": {"M": ["{} משחק בענף הספורט", "ענף הספורט בו משתתף {} הוא", "{} מקושר לענף הספורט"],
+                                     "F": ["{} משחקת בענף הספורט", "ענף הספורט בו משתתפת {} הוא",
+                                           "{} מקושרת לענף הספורט"]},
+                              "ar": {"M": ["", "", ""],
+                                     "F": ["", "", ""]}
+                              }
                     }
 
 OCCUPATION_LANGS = {"Q33999":
@@ -132,25 +135,36 @@ OCCUPATION_LANGS = {"Q33999":
 CLIENT = Client()
 
 # LANGS = ["en", "fr", "ru", "he", "ar"]
-LANGS = ["en", "fr", "he"]
+LANGS = ["en", "fr", "he"]  # TODO: FOR DEBUG - delete
 
-RAW_ENTITIES_PATH = "Dataset/QueiriesData/Entities/raw_entities.json"
+LANG2QID = {"en": "Q1860", "fr": "Q150", "he": "Q9288", "ar": "Q13955", "ru": "Q7737"}
+
+RAW_DATA_PATH = "Dataset/QueiriesData/Entities/raw_data.json"
+
+ENTITIES2LABELS_PATH = "Dataset/ENTITIES2LABELS.json"
+
+with open(ENTITIES2LABELS_PATH, 'r') as file:
+    ENTITIES2LABELS = json.load(file)
 
 
 # ===============================      Global functions:      ===============================
-
 
 def get_entity_name(entity_id, lang):
     """
     :return: The entity name as a string for a given entity id and language code
     """
+    if entity_id in ENTITIES2LABELS and lang in ENTITIES2LABELS[entity_id]:
+        return ENTITIES2LABELS[entity_id][lang]
     try:
         entity = CLIENT.get(entity_id, load=True)
     except Exception as e:
         sys.stderr.write("\n Error:" + str(e) + "\n")
         return -1
     if lang in entity.data['labels']:
-        return entity.data['labels'][lang]['value']
+        if entity_id not in ENTITIES2LABELS:
+            ENTITIES2LABELS[entity_id] = dict()
+        ENTITIES2LABELS[entity_id][lang] = entity.data['labels'][lang]['value']
+        return ENTITIES2LABELS[entity_id][lang]
     else:
         return -1
 
@@ -173,9 +187,11 @@ def merge_json_files(input_files, output_file):
 
 def query_online(sparqlQuery):
     sparql = SPARQL("https://query.wikidata.org/sparql")
-    qlod = sparql.queryAsListOfDicts(sparqlQuery)
-    csv = CSV.toCSV(qlod)
-    return pd.read_csv(io.StringIO(csv))
+    res = sparql.queryAsListOfDicts(sparqlQuery)
+    # === CSV Format: ===
+    # csv = CSV.toCSV(qlod)
+    # pd.read_csv(io.StringIO(csv))
+    return res
 
 
 def url_to_q_entity(url):
@@ -201,122 +217,167 @@ def load_json_file(output_file):
     return data
 
 
+def save_entities_labels():
+    with open('ENTITIES2LABELS.json', 'w') as file:
+        json.dump(ENTITIES2LABELS, file)
+
+
+def load_query(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return ''.join(lines)
+
+
+def query_to_json(path, query_res):
+    with open(path, 'w') as file:
+        for dictionary in query_res:
+            json.dump(dictionary, file)
+            file.write('\n')
+
+
+def collect_data():
+    # {'people': {'en': 2000, 'fr': 1423, 'he': 803}, 'sport': {'en': 2000, 'fr': 1911, 'he': 159}}
+    raw_data = []
+    limit = "2000"
+    queries = ["people", "sport"]
+    info = {q: dict.fromkeys(LANGS) for q in queries}
+    for query in ["people", "sport"]:
+        raw_query = load_query(f"Dataset/Queries/{query}.txt")
+        for lang in LANGS:
+            format_query = raw_query.replace("{lang}", LANG2QID[lang]).replace("{limit}", limit)
+            result = query_online(format_query)
+            for sample in result:
+                sample["lang_code"] = lang
+                sample["query"] = query
+            info[query][lang] = len(result)
+            assert result
+            raw_data += result
+            print(info)
+    return raw_data
+
+
 # ====================================      Class:      ====================================
 
 
 class DatasetBuilder:
 
-    def __init__(self, raw_entities_path=RAW_ENTITIES_PATH):
+    def __init__(self, raw_entities_path=RAW_DATA_PATH):
         self.data = []
-        self.raw_entities = load_json_file(raw_entities_path)[:10]
-        self.cities = {lang: [] for lang in LANGS}
+        self.raw_data = load_json_file(raw_entities_path)
+        random.shuffle(self.raw_data)  # TODO: For debug
+        self.raw_data = self.raw_data[:20]  # TODO: For debug
+        self.id_count = 1
+        self.cities = {lang: set() for lang in LANGS}
+        self.sports = {lang: set() for lang in LANGS}
 
     def preprocess(self):
-        self.preprocess_entities()
-        # TODO add ids
-
-    def preprocess_entities(self):
-        self.entities_samples = []
-        for entity in self.raw_entities:
-            for lang in LANGS:
-                self.append_birth_year_sample(entity, lang)
-                self.append_death_year_sample(entity, lang)
-                self.append_occupation_sample(entity, lang)
-                self.append_birth_city_sample(entity, lang)
+        self.construct_prompts()
         self.assign_target_labels()
-        # TODO assign false labels
 
-    def save_as_json(self, path):
-        # chunks = [self.dataset[i:i + 50] for i in range(0, len(self.dataset), 50)]
-        # for i, chunk in enumerate(chunks):
-        #     with open(f"QueiriesData/Nobel/nobel_dataset_{i}.json", "w", encoding='utf8') as fp:
-        #         json.dump(chunk, fp, ensure_ascii=False)
-        with open(path, "w", encoding='utf8') as fp:
-            json.dump(self.dataset, fp, ensure_ascii=False)
+    def construct_prompts(self):
+        for sample in self.raw_data:
+            query_type = sample['query']
+            if query_type == 'people':
+                self.preprocess_people_sample(sample)
+            elif query_type == 'sport':
+                self.append_sport_sample(sample)
 
-    def append_birth_year_sample(self, entity, lang):
+    def preprocess_people_sample(self, sample):
+        self.append_birth_year_sample(sample)
+        self.append_death_year_sample(sample)
+        self.append_birth_city_sample(sample)
+
+    def append_sport_sample(self, entity):
         if "birthYear" not in entity:
             return -1
         gender = 'M' if entity["gender"].endswith("Q6581097") else "F"
-        sample = {"lang": lang,
-                  "subj": {"label": entity[f"o_{lang}"],
+        sport_qid = url_to_q_entity(entity["sportType"])
+        obj_true = {lang: get_entity_name(sport_qid, lang) for lang in LANGS}
+        sample = {"id": self.id_count,
+                  "subj": {"labels": {lang: entity[f"s_{lang}"] for lang in obj_true.keys()},
+                           "qid": url_to_q_entity(entity["entitiy"]),
+                           "origin": entity["langcode"],
+                           "gender": gender},
+                  "rel": {"label": "sport",
+                          "qid": "P641"},
+                  "obj_true": {"qid": sport_qid,
+                               "label": obj_true},
+                  "prompt": {lang: PROMPT_TEMPLATES["birth_year"][lang][gender][0].format(entity[f"s_{lang}"])
+                             for lang in obj_true.keys()},
+                  "paraphrase_prompts": {lang: [prompt.format(entity[f"s_{lang}"])
+                                                for prompt in PROMPT_TEMPLATES["birth_year"][lang][gender][1:]]
+                                         for lang in obj_true.keys()}}
+        for lang in obj_true.keys():
+            self.sports[lang].add(obj_true[lang])
+        self.data.append(sample)
+        self.id_count += 1
+
+    def append_birth_year_sample(self, entity):
+        if "birthYear" not in entity:
+            return -1
+        gender = 'M' if entity["gender"].endswith("Q6581097") else "F"
+        sample = {"id": self.id_count,
+                  "subj": {"labels": {lang: entity[f"s_{lang}"] for lang in LANGS},
                            "qid": url_to_q_entity(entity["entitiy"]),
                            "origin": entity["langcode"],
                            "gender": gender},
                   "rel": {"label": "birth_year",
                           "qid": "P569"},
-                  "obj_true": {"label": entity["birthYear"]}}
-        gender = 'M' if entity["gender"].endswith("Q6581097") else "F"
-        prompts = [prompt.format(entity[f"o_{lang}"]) for prompt in PROMPT_TEMPLATES["birth_year"][lang][gender]]
-        # random.shuffle(prompts)
-        sample["prompt"] = prompts[0]
-        sample["paraphrase_prompts"] = prompts[1:]
-        self.entities_samples.append(sample)
+                  "obj_true": {"label": entity["birthYear"]},
+                  "prompt": {lang: PROMPT_TEMPLATES["birth_year"][lang][gender][0].format(entity[f"s_{lang}"])
+                             for lang in LANGS},
+                  "paraphrase_prompts": {lang: [prompt.format(entity[f"s_{lang}"])
+                                                for prompt in PROMPT_TEMPLATES["birth_year"][lang][gender][1:]]
+                                         for lang in LANGS}}
+        self.data.append(sample)
+        self.id_count += 1
 
-    def append_death_year_sample(self, entity, lang):
+    def append_death_year_sample(self, entity):
         if "deathYear" not in entity:
             return -1
         gender = 'M' if entity["gender"].endswith("Q6581097") else "F"
-        sample = {"lang": lang,
-                  "subj": {"label": entity[f"o_{lang}"],
+        sample = {"id": self.id_count,
+                  "subj": {"labels": {lang: entity[f"s_{lang}"] for lang in LANGS},
                            "qid": url_to_q_entity(entity["entitiy"]),
                            "origin": entity["langcode"],
                            "gender": gender},
                   "rel": {"label": "death_year",
                           "qid": "P570"},
-                  "obj_true": {"label": entity["deathYear"]}}
-        prompts = [prompt.format(entity[f"o_{lang}"]) for prompt in PROMPT_TEMPLATES["birth_year"][lang][gender]]
-        # random.shuffle(prompts)
-        sample["prompt"] = prompts[0]
-        sample["paraphrase_prompts"] = prompts[1:]
-        self.entities_samples.append(sample)
+                  "obj_true": {"label": entity["deathYear"]},
+                  "prompt": {lang: PROMPT_TEMPLATES["death_year"][lang][gender][0].format(entity[f"s_{lang}"])
+                             for lang in LANGS},
+                  "paraphrase_prompts": {lang: [prompt.format(entity[f"s_{lang}"])
+                                                for prompt in PROMPT_TEMPLATES["death_year"][lang][gender][1:]]
+                                         for lang in LANGS}}
+        self.data.append(sample)
+        self.id_count += 1
 
-    def append_occupation_sample(self, entity, lang):
-        if "occupation" not in entity:
-            return -1
-        gender = 'M' if entity["gender"].endswith("Q6581097") else "F"
-        sample = {"lang": lang,
-                  "subj": {"label": entity[f"o_{lang}"],
-                           "qid": url_to_q_entity(entity["entitiy"]),
-                           "origin": entity["langcode"],
-                           "gender": gender},
-                  "rel": {"label": "occupation",
-                          "qid": "P106"},
-                  "obj_true": {"label": OCCUPATION_LANGS[url_to_q_entity(entity["occupation"])][lang][gender],
-                               "qid": url_to_q_entity(entity["occupation"])}}
-        prompts = [prompt.format(entity[f"o_{lang}"]) for prompt in PROMPT_TEMPLATES["occupation"][lang][gender]]
-        # random.shuffle(prompts)
-        sample["prompt"] = prompts[0]
-        sample["paraphrase_prompts"] = prompts[1:]
-        self.entities_samples.append(sample)
-
-    def append_birth_city_sample(self, entity, lang):
+    def append_birth_city_sample(self, entity):
         if "cityOfBirth" not in entity:
             return -1
+        city_qid = url_to_q_entity(entity["cityOfBirth"])
+        obj_true = {lang: get_entity_name(city_qid, lang) for lang in LANGS}
         gender = 'M' if entity["gender"].endswith("Q6581097") else "F"
-        birth_city = get_entity_name(url_to_q_entity(entity["cityOfBirth"]), lang)
-        if birth_city == -1:
-            return -1
-        sample = {"lang": lang,
-                  "subj": {"label": entity[f"o_{lang}"],
+        sample = {"id": self.id_count,
+                  "subj": {"labels": {lang: entity[f"s_{lang}"] for lang in obj_true.keys()},
                            "qid": url_to_q_entity(entity["entitiy"]),
                            "origin": entity["langcode"],
                            "gender": gender},
-                  "rel": {"label": "birth_city",
-                          "qid": "P19"},
-                  "obj_true": {"label": birth_city,
-                               "qid": url_to_q_entity(entity["cityOfBirth"])}
-                  }
-        prompts = [prompt.format(entity[f"o_{lang}"]) for prompt in PROMPT_TEMPLATES["birth_city"][lang][gender]]
-        # random.shuffle(prompts)
-        sample["prompt"] = prompts[0]
-        sample["paraphrase_prompts"] = prompts[1:]
-        self.entities_samples.append(sample)
-        self.cities[lang].append(sample["obj_true"])
+                  "rel": {"label": "birth_city", "qid": "P19"},
+                  "obj_true": {"label": obj_true, "qid": city_qid},
+                  "prompt": {lang: PROMPT_TEMPLATES["birth_city"][lang][gender][0].format(entity[f"s_{lang}"])
+                             for lang in obj_true.keys()},
+                  "paraphrase_prompts": {lang: [prompt.format(entity[f"s_{lang}"])
+                                                for prompt in PROMPT_TEMPLATES["birth_city"][lang][gender][1:]]
+                                         for lang in obj_true.keys()}}
+        for lang in obj_true.keys():
+            self.cities[lang].add(obj_true[lang]) # TODO: Change
+        self.data.append(sample)
+        self.id_count += 1
 
     def assign_target_labels(self):
 
-        for sample in self.entities_samples:
+        for sample in self.data:
             diff = random.randint(1, 30)
             sign = random.randint(0, 1)
             final = diff if sign else -diff
@@ -341,7 +402,20 @@ class DatasetBuilder:
                 else:
                     sample["target_true"] = rel_occupations[1]
 
+    def save(self, path):
+        with open(path, "w", encoding='utf8') as fp:
+            json.dump(self.dataset, fp, ensure_ascii=False)
+
 
 def main():
     db = DatasetBuilder()
     db.preprocess()
+    save_entities_labels()
+
+
+    # raw_data = collect_data()
+    # with open('raw_data.json', 'w') as file:
+    #     for dictionary in raw_data:
+    #         json.dump(dictionary, file)
+    #         file.write('\n')
+    # exit(0)
