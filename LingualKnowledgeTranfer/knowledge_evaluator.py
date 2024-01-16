4f13c9fbd2dd60f3ce073d8638521da0df051ee1
maxim <maxim758@gmail.com>
2024-01-16 15:26:23 +0200

    Eval try

--- LingualKnowledgeTranfer/knowledge_evaluator.py
+++ LingualKnowledgeTranfer/knowledge_evaluator.py
@@ -49,6 +49,7 @@ class KnowledgeEvaluator:
                     max_new_tokens=3
                 )
                 pred = self.tok.decode(model_output.detach().cpu().numpy().tolist()[0])
+                print(pred)
                 results.append([sample_id, lang, pred, gold])
         final_results = pd.DataFrame(results, columns=["id", "lang", "pred", "gold"])
         eval_result = evaluate_metrics(final_results["gold"], final_results["pred"])
