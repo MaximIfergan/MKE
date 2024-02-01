08074ab1c662592b2bc1fdf9950b94f6bae0355a
maxim <maxim758@gmail.com>
2024-02-01 03:40:15 +0200

    background try

--- LingualKnowledgeTranfer/model_editor.py
+++ LingualKnowledgeTranfer/model_editor.py
@@ -124,6 +124,8 @@ class KnowledgeEditor():
                                                   trust_remote_code=True)
 
         results = dict() if not res_path else load_json_file(res_path)[0]
+        print(f"size of known_facts: {len(self.known_facts)}")
+        self.known_facts = self.known_facts[:20]
         for i, sample in tqdm(enumerate(self.known_facts), total=len(self.known_facts)):
 
             # === save temp results in crash case:
