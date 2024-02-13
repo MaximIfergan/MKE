01a47c097b9ece18981223cd930a5dc0a1607bd5
maxim <maxim758@gmail.com>
2024-02-13 18:37:05 +0200

    Model evaluation try

--- LingualKnowledgeTransfer/knowledge_evaluator.py
+++ LingualKnowledgeTransfer/knowledge_evaluator.py
@@ -334,9 +334,9 @@ class KnowledgeEvaluator:
 
 def main():
     # "Qwen/Qwen-7B", "meta-llama/Llama-2-7b", "bigscience/bloom-7b1"
-    for model_name in ["meta-llama/Llama-2-7b"]:
+    for model_name in ["Qwen/Qwen-7B", "bigscience/bloom-7b1"]:
         ke = KnowledgeEvaluator(exp_name=f"model_try_{model_name}")
-        ke.eval(model_name=model_name, n_samples=20)
+        ke.eval(model_name=model_name, n_samples=500)
         ke.save_results()
 
     # ke.append_metadata_info()
