d175bd1764ef33a2b975c9c9856fb60789117ef3
maxim <maxim758@gmail.com>
2024-03-06 15:24:20 +0200

    Pushing changes from last experiment

--- LingualKnowledgeTransfer/knowledge_editor.py
+++ LingualKnowledgeTransfer/knowledge_editor.py
@@ -71,7 +71,9 @@ class KnowledgeEditor():
         if 'qwen' in self.model_name.lower():
             # hparams = ROMEHyperParams.from_hparams("EasyEdit/hparams/ROME/qwen-7b.yaml")
             # tokenizer.pad_token = "<|endoftext|>"
-            hparams = MEMITHyperParams.from_hparams("EasyEdit/hparams/MEMIT/qwen-7b.yaml")
+
+            # hparams = MEMITHyperParams.from_hparams("EasyEdit/hparams/MEMIT/qwen-7b.yaml")
+            hparams = MEMITHyperParams.from_hparams("EasyEdit/hparams/MEMIT/mistral-7b.yaml")
             tokenizer.pad_token = "<|endoftext|>"
 
         known_facts = self.known_facts
