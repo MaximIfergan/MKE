17b2004c493396bc444346fc9eff866e9b19b1f4
maxim <maxim758@gmail.com>
2024-03-17 16:40:28 +0200

    Documentation to the editing analysis + Design + Efficacy

--- LingualKnowledgeTransfer/knowledge_editor.py
+++ LingualKnowledgeTransfer/knowledge_editor.py
@@ -315,7 +315,7 @@ class KnowledgeEditor():
 
 def main():
     for exp in [
-                ("Qwen", "Experiments/07-03-meeting/Qwen_edition.json", "Experiments/12-02-meeting/qwen_evaluation.csv", "Qwen/Qwen-7B"),
+                # ("Qwen", "Experiments/07-03-meeting/Qwen_edition.json", "Experiments/12-02-meeting/qwen_evaluation.csv", "Qwen/Qwen-7B"),
                 ("Mistral", "", "Experiments/12-02-meeting/qwen_evaluation.csv", "mistralai/Mistral-7B-v0.1"),
                 # ("Bloom", "Experiments/17-01-meeting/mke_edition.json", "Experiments/17-01-meeting/mke_evaluation.csv", "bigscience/bloom-7b1")
                 ]:
