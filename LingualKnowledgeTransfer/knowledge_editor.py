c2146d9ea2e38dd81a389964d018413aef6ae641
maxim <maxim758@gmail.com>
2024-03-20 13:36:05 +0200

    Save MEMIT Cov Qwen

--- LingualKnowledgeTransfer/knowledge_editor.py
+++ LingualKnowledgeTransfer/knowledge_editor.py
@@ -170,7 +170,7 @@ class KnowledgeEditor():
                 model_output = edited_model.generate(
                     input_ids=batch_tok['input_ids'].to('cuda:0'),
                     attention_mask=batch_tok['attention_mask'].to('cuda:0'),
-                    max_new_tokens=5,
+                    max_new_tokens=8,
                     # pad_token_id = tokenizer.eos_token_id if 'mistral' in self.model_name.lower() else None
                 )
 
